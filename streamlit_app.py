069392a71c2d7f1891f6b78f8282f4df12b5b765
KhutsishviliS <sabakhutsishvili846@gmail.com>
2024-09-24 19:16:30 +0200

    V1.0.2.3.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 from streamlit_option_menu import option_menu
+import base64
+
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+# Set up the background image in Streamlit page design
 def page_design():
-    page_bg_img ="""
+    img_base64 = get_img_as_base64("images/magicpattern1.png")
+    page_bg_img = f"""
     <style>
-    [class="main st-emotion-cache-bm2z3a ea3mdgi8"]
-    {background-color: #274f69;
-    }
-    [data-testid="stAppViewBlockContainer"]{
-    background-color: #FFF
-    }
+    .main {{
+        background-color: #274f69;
+        
+    }}
+    [data-testid="stVerticalBlock"] {{
+        background-color: white;
+    }}
+    [data-testid="stAppViewBlockContainer"]{{
+        
+        background-color: white;
 
-    [data-testid="stHeader"]{
-    background-color: #FE6F00
-    }
-    [data-testid="stSidebarContent"]{
-    background-color: white
-    }
+    }}
+    [data-testid="stHeader"] {{
+        background-color: #FE6F00;
+    }}
+    [data-testid="stSidebarContent"] {{
+        background-color: white;
+    }}
     </style>
-
-
     """
-    st.markdown(page_bg_img,unsafe_allow_html=True)
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 page_design()
 ###############################################
