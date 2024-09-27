5fb2dd3b5161285e7a7ba985e939fbd0ddc6e7e7
KhutsishviliS <sabakhutsishvili846@gmail.com>
2024-09-27 14:21:13 +0200

    Update mnist.py

--- page_list/mnist.py
+++ page_list/mnist.py
@@ -4,7 +4,7 @@ import streamlit as st
 language = st.session_state.get('language', 'Georgian')
 
 if language =="Georgian":
-    st.markdown('<h1 style="color:#3A485F;">გამარჯობა ⚙️</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="color:#3A485F;">Ooops ⚙️</h1>', unsafe_allow_html=True)
     st.markdown(""" <p style="color: #3A485F;">
     გმადლობთ რომ დაინტერესებული ხართ ჩემი ბლოგით. ამ გვერდზე მიმდინარეობს მუშაობა და მალე მზად იქნება  
     </p>""",unsafe_allow_html=True)
@@ -12,7 +12,7 @@ if language =="Georgian":
     <a href="https://learndeeplearningwithsaba.streamlit.app/" target="_self">მთავარი გვერდი</button></a>
     """
 else:
-    st.markdown('<h1 style="color:#3A485F;">Hello ⚙️</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="color:#3A485F;">Ooops ⚙️</h1>', unsafe_allow_html=True)
     st.markdown(""" <p style="color: #3A485F;">
     Thank you for your interest in my blog. This page is under construction and will be ready soon.
     </p>""",unsafe_allow_html=True)
